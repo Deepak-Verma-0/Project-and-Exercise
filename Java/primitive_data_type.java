ab2d6a341020a6945fb7822ba252c098a80a04ec
Deepak-Verma-0 <143989151+Deepak-Verma-0@users.noreply.github.com>
2023-09-16 07:41:15 +0000

    add file

---
+++ Java/primitive_data_type.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class primitive_data_type{
+    public static void main(String[] args){
+        //byte -1[-128 to 127]
+        //short - 2
+        // int- 4
+        //long- 8
+        // float- 4
+        // double- 8
+        // char- 2
+        // boolean- 1 true/ false
+
+        byte age = 30;
+        int phone = 1234342444;
+        long phone2 = 1345433554543L;    
+        boolean isAdult = true;
+    }
+}
