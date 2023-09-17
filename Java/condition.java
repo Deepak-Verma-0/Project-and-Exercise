bdbe93549540bbc5ff55d10691767684ece7d224
Deepak-Verma-0 <143989151+Deepak-Verma-0@users.noreply.github.com>
2023-09-17 19:09:29 +0000

    add

---
+++ Java/condition.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class condition{
+    public  static void main(String[] args){
+        Scanner  sc = new Scanner(System.in);
+        // conditional statement
+        
+       // int age =30;
+        //if(age>=30)
+        //    System.out.println("you vote");
+        //if(age>=30)
+          //  System.out.println("you vote");
+        //else
+          //  System.out.println("not vote");
+
+        //loops
+
+        for(int i=0;i>=100;i++){
+            System.out.print(i);
+        }
+
+        int j=0;
+        while(j>=50){
+            System.out.println(j);
+            j++;
+        }
+        
+    }
+}
--- Java/jmath.java
+++ Java/jmath.java
@@ -1,8 +1,19 @@
 import java.util.*;
 public class jmath{
-    public static void maing(String[] args){
-        // math
-        int mynum= 5;
-        System.out.println(math.max(5,6));
+    public static void main(String[] args){
+        //maths
+        //5,6
+        System.out.println(Math.max(5,6));
+        System.out.println(Math.min(5,7));
+
+        // random number
+        //when we need to automatic random number
+        System.out.println((int)(Math.random()*100));
+        Scanner scs= new Scanner(System.in);
+       // int age=  scs.nextInt();
+        //System.out.println("your age " + age);
+
+        String name= scs.nextLine();
+        System.out.println( name);  
     }
 }
