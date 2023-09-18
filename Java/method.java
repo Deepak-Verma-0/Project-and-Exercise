267064d25b0452a82640a3b7cd774920d00113cf
Deepak-Verma-0 <143989151+Deepak-Verma-0@users.noreply.github.com>
2023-09-18 07:14:26 +0000

    add

---
+++ Java/method.java
@@ -0,0 +1,11 @@
+public class method{
+    
+    //method 
+        public static void printJava(){
+            System.out.println("hello world");
+        }
+    public static void main(String[] args){
+        printJava();
+        printJava();
+    }
+}
