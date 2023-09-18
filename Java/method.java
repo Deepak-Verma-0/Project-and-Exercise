3ce9ac6835271d2b31e8a07124cb599080cd09a9
Deepak-Verma-0 <143989151+Deepak-Verma-0@users.noreply.github.com>
2023-09-18 07:23:11 +0000

    add

--- Java/method.java
+++ Java/method.java
@@ -4,8 +4,23 @@ public class method{
         public static void printJava(){
             System.out.println("hello world");
         }
+
+        public static void printName(String name){
+            System.out.println(name);
+        }
+
+        public static void printSum(int  a,int b){
+            int  sum=a+b;
+            System.out.println(sum);
+        }
+
+
     public static void main(String[] args){
         printJava();
         printJava();
+        printName("deepak verma");
+
+        printSum(2,3);
+        printSum(5,4);
     }
 }
