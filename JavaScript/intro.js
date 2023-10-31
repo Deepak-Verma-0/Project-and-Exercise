13243c7515dc2b7455b95059f699e10d7bc552eb
Deepak verma <dv226222@gmail.com>
2023-10-31 12:04:24 +0530

    add

---
+++ JavaScript/intro.js
@@ -0,0 +1,26 @@
+let x=5;
+let y=6;
+let z=x+y;
+
+document.getElementById("demo").innerHTML="the value of z is "+ z;
+
+console.log(z);
+
+// javaScipt Variables
+
+const price1 =5;
+const price2 =6;
+
+let total= price1 + price2;
+
+document.getElementById("p2").innerHTML="The total is: "+ total;
+
+
+// many variables is one statement
+
+let persont="johne deo",carName="volve",price="200";
+document.getElementById("var").innerHTML=carName + " "+ price;
+
+document.write(persont);
+//window.alert("hello");
+console.log("price");
